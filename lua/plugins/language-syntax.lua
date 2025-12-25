c436589fb2882efa0d831831747f086c22872d90
takid1412 <duanpq92@gmail.com>
2025-12-25 18:31:29 +0700

    add openresty

--- lua/plugins/language-syntax.lua
+++ lua/plugins/language-syntax.lua
@@ -1,14 +1,12 @@
 return {
     { "vim-scripts/iptables" },
     {
-        "chr4/nginx.vim",
-        config = function()
-            vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-                pattern = "/etc/nginx/**/*.lua",
-                callback = function()
-                    vim.bo.filetype = "lua"
-                end,
-            })
-        end,
+        -- "takid1412/nvim-treesitter-openresty",
+        dir = "~/Dev/OtherProjects/nvim-treesitter-nginx",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+        lazy = false,
+        build = ":TSUpdate nginx",
     },
 }
